69fb5874481aa3ca71270c0f044da1503c7ba454
cjdjr <2609943977@qq.com>
2021-08-27 15:17:16 +0800

    rename gridsim

--- gridsim-master-0811/unittest/README.md
+++
@@ -1,3 +0,0 @@
-# How to add a new unittest
-
-Add the file with the naming `*_test.py` in unittest directory.
--- gridsim-master-0811/utilize/form_action.py
+++
@@ -1,2 +0,0 @@
-def form_action(adjust_gen_p, adjust_gen_v):
-    return {'adjust_gen_p': adjust_gen_p, 'adjust_gen_v': adjust_gen_v}
